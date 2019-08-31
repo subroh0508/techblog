0718bfe2f6969a579248f468340da46094cb1897
subroh0508 <subroh.0508@gmail.com>
2019-09-01 01:51:28 +0900

    [edit]Add redirectTo method to Header.vue

--- src/components/global/header/Header.vue
+++ src/components/global/header/Header.vue
@@ -1,2 +1,13 @@
+<script>
+import router from '../../../router';
+
+export default {
+  methods: {
+    redirectTo: (name) => {
+      router.push({ name });
+    },
+  },
+}
+</script>
 <template src='./header.html'/>
 <style src='./header.scss'/>
