7f8bf01d04a561bc54975ebf7d432ecab7ff6057
subroh0508 <subroh.0508@gmail.com>
2019-09-08 16:48:52 +0900

    [add]Add md2json.js

--- articles/test.md
+++ articles/test.md
@@ -1,10 +1,10 @@
 ---
 title: test-1
-display_title: テスト
+displayTitle: テスト
 description: とりあえずのテストページだお
-published_at: 2019-09-01T12:00:00+09:00
-updated_at: 2019-09-01T12:00:00+09:00
+publishedAt: 2019-09-01T12:00:00+09:00
 tags:
   - aaa
   - bbb
 ---
+本日天気晴朗なれども波高し
---
+++ packages/articles/md2json.js
@@ -0,0 +1,20 @@
+import { promises } from 'fs';
+import fm from 'front-matter';
+
+export default async path => {
+  const [content, fileMeta] = await Promise.all([promises.readFile(path, 'utf8'), promises.stat(path)]);
+
+  const frontMatter = fm(content);
+  const {
+    publishedAt = fileMeta.birthtime.toISOString(),
+    updatedAt = fileMeta.mtime.toISOString(),
+    author = 'subroh_0508',
+  } = frontMatter.attributes;
+
+  return Object.assign({}, frontMatter.attributes, {
+    publishedAt: new Date(publishedAt).toISOString(),
+    updatedAt: new Date(updatedAt).toISOString(),
+    author,
+    body: frontMatter.body,
+  });
+};
