50390ae023d2e04a57b487842e6db8ef7864150d
subroh0508 <subroh.0508@gmail.com>
2019-09-15 18:32:01 +0900

    [edit]Enable normal OGP

--- packages/blog/components/insertMetaTags.js
+++ packages/blog/components/insertMetaTags.js
@@ -20,38 +20,64 @@ export default (to) => {
   }
 }
 
-const forHome = () => {
-  document.title = '横須賀第765管区情報局'
-  document.querySelector("meta[name='description']")
-    .setAttribute('content', 'Subroh Nishikoriの技術ブログ');
-}
+const SITE_NAME = '横須賀第765管区情報局';
+
+const TYPE_WEBSITE = 'website';
+const TYPE_ARTICLE = 'article';
+
+const forHome = () => setAllAttributes(
+  TYPE_WEBSITE,
+  SITE_NAME,
+  'Subroh Nishikoriの技術ブログ',
+);
 
 const forArticles = (query) => {
   if (query.tag) {
-    document.title = `タグ検索: ${query.tag} - 横須賀第765管区情報局`;
-    document.querySelector("meta[name='description']")
-      .setAttribute('content', `${query.tag}タグ付きの全ての記事`);
+    setAllAttributes(
+      TYPE_WEBSITE,
+      `タグ検索: ${query.tag} - ${SITE_NAME}`,
+      `${query.tag}タグ付きの全ての記事`,
+    );
   } else {
-    document.title = '全ての記事 - 横須賀第765管区情報局';
-    document.querySelector("meta[name='description']")
-      .setAttribute('content', '全ての記事');
+    setAllAttributes(
+      TYPE_WEBSITE,
+      `全ての記事 - ${SITE_NAME}`,
+      '全ての記事',
+    );
   }
 }
 
-const forAbout = () => {
-  document.title = 'このサイトについて - 横須賀第765管区情報局'
-  document.querySelector("meta[name='description']")
-    .setAttribute('content', 'このサイトや筆者について');
-}
+const forAbout = () => setAllAttributes(
+  TYPE_WEBSITE,
+  `このサイトについて - ${SITE_NAME}`,
+  'このサイトや筆者について',
+);
 
-const forLinks = () => {
-  document.title = 'リンク集 - 横須賀第765管区情報局'
-  document.querySelector("meta[name='description']")
-    .setAttribute('content', '筆者のネットにおける位置情報');
-}
+const forLinks = () => setAllAttributes(
+  TYPE_WEBSITE,
+  `リンク集 - ${SITE_NAME}`,
+  '筆者のネットにおける位置情報',
+);
 
-const forArticle = ({ displayTitle, description }) => {
-  document.title = `${displayTitle} - 横須賀第765管区情報局`
+const forArticle = ({ displayTitle, description }) => setAllAttributes(
+  TYPE_ARTICLE,
+  `${displayTitle} - ${SITE_NAME}`,
+  description,
+);
+
+const setAllAttributes = (
+  type, title, description,
+) => {
+  document.title = title;
   document.querySelector("meta[name='description']")
-    .setAttribute('content', description);
+    .setAttribute('content', description)
+
+  document.querySelector("meta[property='og:type']")
+    .setAttribute('content', type)
+  document.querySelector("meta[property='og:title']")
+    .setAttribute('content', title)
+  document.querySelector("meta[property='og:description']")
+    .setAttribute('content', description)
+  document.querySelector("meta[property='og:url']")
+    .setAttribute('content', window.location.href)
 }
