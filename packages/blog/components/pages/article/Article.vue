b34eccb854634f42de422401ee9932adb9b3f79f
subroh0508 <subroh.0508@gmail.com>
2019-12-24 01:07:09 +0900

    draft

--- packages/blog/components/pages/article/Article.vue
+++ packages/blog/components/pages/article/Article.vue
@@ -109,5 +109,19 @@ export default {
     padding: 10px 20px;
     border-radius: 4px;
   }
+  .movie {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-top: 56.25%;
+
+    > iframe {
+      position: absolute;
+      top:0;
+      left:0;
+      width: 100%;
+      height: 100%;
+    }
+  }
 }
 </style>
