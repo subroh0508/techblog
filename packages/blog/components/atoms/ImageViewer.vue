5c07d98ff369de010f45d5fbada28637e7728653
subroh0508 <subroh.0508@gmail.com>
2020-08-25 23:31:30 +0900

    [add]Add ImageViewer to atoms

---
+++ packages/blog/components/atoms/ImageViewer.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  props: {
+    src: String,
+    alt: String,
+  },
+}
+</script>
+<template>
+  <div class='image-viewer'>
+    <img src="src" alt="alt"/>
+  </div>
+</template>
+<style scoped lang='scss'>
+@import '~@components/_breakpoints';
+
+.image-viewer {
+  background-color: transparent;
+  width: 100%;
+  object-fit: cover;
+
+  > img {
+    width: 1000px;
+    background-color: transparent;
+    object-fit: cover;
+  }
+
+  @media screen and (max-width: $breakpoint-md) {
+    > img {
+      position: relative;
+      width: 80%;
+
+      &::before {
+        display: block;
+        width: 100%;
+        padding-bottom: 100%;
+        content: "";
+      }
+    }
+  }
+
+  @media screen and (max-width: $breakpoint-lg) {
+    > img {
+      width: 800px;
+    }
+  }
+}
+</style>
