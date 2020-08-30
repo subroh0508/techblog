1926a1d3258b945a27e0df0720e3e775f0c8ad08
subroh0508 <subroh.0508@gmail.com>
2020-08-30 12:18:56 +0900

    [fix]Change existsThumbnail function synchronously

--- packages/articles/scripts/existsThumbnail.js
+++ packages/articles/scripts/existsThumbnail.js
@@ -1,13 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
-const fsp = fs.promises;
-
-export default async filename => {
+export default filename => {
   const thumbnail = path.resolve(__dirname, `../../../articles/thumbs/${filename}`);
 
   try {
-    await fsp.access(thumbnail);
+    fs.accessSync(thumbnail);
     return true;
   } catch (e) {
     return false;
