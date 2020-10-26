7c72ece819678168909aebb78f94fe93be301f13
subroh0508 <subroh.0508@gmail.com>
2020-10-27 00:11:08 +0900

    [edit]Rewrite router.js

--- packages/blog/router.js
+++ packages/blog/router.js
@@ -1,16 +1,13 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import { createRouter as createVueRouter, createWebHistory } from 'vue-router';
 
-Vue.use(VueRouter);
-
-export const createRouter = () => new VueRouter({
-  mode: 'history',
+export const createRouter = () => createVueRouter({
+  history: createWebHistory(),
   routes: [
     { path: '/', name: 'home', component: () => import('./components/pages/home/Home') },
     { path: '/articles', name: 'articles', component: () => import('./components/pages/article/Articles') },
     { path: '/articles/:title', name: 'article', component: () => import('./components/pages/article/Article'), props: true },
     { path: '/about', name: 'about', component: () => import('./components/pages/about/About') },
-    { path: '*', component: () => import('./components/pages/global/NotFound') },
+    { path: '/:catchAll(.*)', component: () => import('./components/pages/global/NotFound') },
   ],
   scrollBehavior (to, from, savedPosition) {
     return { x: 0, y: 0 };
