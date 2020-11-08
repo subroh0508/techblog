12ca8a34deab1c872d6a072ae7d6b9395fd428fb
subroh0508 <subroh.0508@gmail.com>
2020-11-08 21:38:45 +0900

    [edit]Use defineAsyncComponent on router

--- packages/blog/router.js
+++ packages/blog/router.js
@@ -1,13 +1,14 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter as createVueRouter, createWebHistory, createMemoryHistory } from 'vue-router';
 
 const createRouter = (history) => createVueRouter({
   history,
   routes: [
-    { path: '/', name: 'home', component: () => import('./components/pages/home/Home') },
-    { path: '/articles', name: 'articles', component: () => import('./components/pages/article/Articles') },
-    { path: '/articles/:title', name: 'article', component: () => import('./components/pages/article/Article'), props: true },
-    { path: '/about', name: 'about', component: () => import('./components/pages/about/About') },
-    { path: '/:catchAll(.*)', component: () => import('./components/pages/global/NotFound') },
+    { path: '/', name: 'home', component: defineAsyncComponent(() => import('./components/pages/home/Home')) },
+    { path: '/articles', name: 'articles', component: defineAsyncComponent(() => import('./components/pages/article/Articles')) },
+    { path: '/articles/:title', name: 'article', component: defineAsyncComponent(() => import('./components/pages/article/Article')), props: true },
+    { path: '/about', name: 'about', component: defineAsyncComponent(() => import('./components/pages/about/About')) },
+    { path: '/:catchAll(.*)', component: defineAsyncComponent(() => import('./components/pages/global/NotFound')) },
   ],
   scrollBehavior (to, from, savedPosition) {
     return { left: 0, top: 0 };
