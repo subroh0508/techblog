88c3c175f0ada9800247396d84f314ac63bc34dc
subroh0508 <subroh.0508@gmail.com>
2020-11-15 23:24:54 +0900

    [edit]Rename app -> server

--- packages/blog/functions/src/server.js
+++ packages/blog/functions/src/server.js
@@ -2,29 +2,30 @@ import express from 'express';
 import path from 'path';
 import { renderToString } from '@vue/server-renderer';
 import serverBundle from '../assets/server.bundle.js';
+import template from '../assets/index.html';
 
 import metatags from './metatags';
 
-const app = express();
+const server = express();
+const { app, router } = serverBundle();
 
 if (process.env.NODE_ENV === 'development') {
-  app.use(express.static(path.resolve(__dirname, '../../build')));
-  app.use(express.static(path.resolve(__dirname, '../../public')));
+  server.use(express.static(path.resolve(__dirname, '../../build')));
+  server.use(express.static(path.resolve(__dirname, '../../public')));
 }
 
-app.get('*', async (req, res) => {
+server.get('*', async (req, res) => {
   const context = { ...metatags(req.path, req.query), url: req.url };
-  const { app, router } = serverBundle();
 
   router.push(context.url);
   await router.isReady();
   const html = await renderToString(app, context);
 
-  res.end(html);
+  res.end(template.replace('<!--vue-ssr-outlet-->', html));
 });
 
 if (process.env.NODE_ENV === 'development') {
-  app.listen(8080, () => console.log('listening port: 8080'));
+  server.listen(8080, () => console.log('listening port: 8080'));
 }
 
-export default app;
+export default server;
