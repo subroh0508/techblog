96f1812a3d44efc3de665b92ff48cdc067ae66ec
subroh0508 <subroh.0508@gmail.com>
2020-11-16 02:36:19 +0900

    [fix]Move HtmlWebpackPlugin to webpack.client-common.js

--- packages/blog/scripts/webpack.client-common.js
+++ packages/blog/scripts/webpack.client-common.js
@@ -1,6 +1,13 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.common.js');
+
+const mode = process.env.NODE_ENV || 'development';
+const dev = mode === 'development';
+const htmlOutputPath = dev ?
+  path.resolve(__dirname, '../build/index.html') :
+  path.resolve(__dirname, '../functions/assets/index.html');
 
 module.exports = merge(common, {
   output: {
@@ -13,4 +20,12 @@ module.exports = merge(common, {
       vue: '@vue/runtime-dom/dist/runtime-dom.esm-bundler.js',
     },
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../index.template.ejs'),
+      filename: htmlOutputPath,
+      templateParameters: { dev },
+      minify: { removeComments: false },
+    }),
+  ],
 });
--- packages/blog/scripts/webpack.common.js
+++ packages/blog/scripts/webpack.common.js
@@ -1,13 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
 const { VueLoaderPlugin } = require('vue-loader');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 const dev = mode === 'development';
-const htmlOutputPath = dev ?
-  path.resolve(__dirname, '../build/index.html') :
-  path.resolve(__dirname, '../functions/assets/index.html');
 
 module.exports = {
   resolve: {
@@ -35,12 +31,6 @@ module.exports = {
   },
   plugins: [
     new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../index.template.ejs'),
-      filename: htmlOutputPath,
-      templateParameters: { dev },
-      minify: { removeComments: false },
-    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: `"${mode}"`,
