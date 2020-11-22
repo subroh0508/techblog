698f051ac1d7ab1e87838854f77600f64cecaace
subroh0508 <subroh.0508@gmail.com>
2020-11-22 23:30:48 +0900

    [fix]Add makeFunctionsPackageJson to blog/scripts

---
+++ packages/blog/scripts/makeFunctionsPackgeJson.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+
+const fsp = fs.promises;
+
+const make = async () => {
+  const rawJson = await fsp.readFile(path.resolve(__dirname, '../package.json'));
+  const json = JSON.parse(rawJson);
+  
+  const output = {
+    name: 'functions',
+    description: 'Cloud Functions for Firebase',
+    main: 'index.js',
+    engines: { node: '12' },
+    dependencies: json.dependencies,
+    private: true,
+  }
+  
+  await fsp.writeFile(path.resolve(__dirname, '../build/package.json'), JSON.stringify(output));
+};
+
+make()
+  .then(() => console.log('completed!'))
+  .catch(e => console.error(e));
