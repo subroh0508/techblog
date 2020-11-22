383f8e92845e34d5e8063e08565a0ac6268c1426
subroh0508 <subroh.0508@gmail.com>
2020-11-22 23:39:04 +0900

    [fix]Filter '@techblog/articles' from package.json of dependencies

--- packages/blog/scripts/makeFunctionsPackgeJson.js
+++ packages/blog/scripts/makeFunctionsPackgeJson.js
@@ -6,13 +6,13 @@ const fsp = fs.promises;
 const make = async () => {
   const rawJson = await fsp.readFile(path.resolve(__dirname, '../package.json'));
   const json = JSON.parse(rawJson);
-  
+
   const output = {
     name: 'functions',
     description: 'Cloud Functions for Firebase',
     main: 'index.js',
     engines: { node: '12' },
-    dependencies: json.dependencies,
+    dependencies: json.dependencies.filter(d => d !== '@techblog/articles'),
     private: true,
   }
   
