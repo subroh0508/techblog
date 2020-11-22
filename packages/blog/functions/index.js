657c8d8fa33b69392975b10c5d8d4df01bca0d56
subroh0508 <subroh.0508@gmail.com>
2020-11-22 22:33:52 +0900

    [fix]Fix GitHub actions workflow

--- packages/blog/functions/index.js
+++
@@ -1,4 +0,0 @@
-import { https } from 'firebase-functions';
-import app from './src/server';
-
-export const ritsuko = https.onRequest(app);
--- packages/functions/index.js
+++
@@ -1,4 +0,0 @@
-import { https } from 'firebase-functions';
-import app from '../blog/build/server/server.bundle.js';
-
-export const ritsuko = https.onRequest(app);
