53ce49e92bb2bcc1793301b81e866bc3ebbefb1f
subroh0508 <subroh.0508@gmail.com>
2021-06-20 19:07:01 +0900

    [add]Add NavMenu.stories.js

---
+++ packages/blog/components/atoms/NavMenu.stories.js
@@ -0,0 +1,21 @@
+import NavMenuComponent from '@components/atoms/NavMenu';
+
+export default {
+  title: 'Components/atoms/NavMenu',
+  component: NavMenuComponent,
+}
+
+export const NavMenu = (args) => ({
+  components: { NavMenuComponent },
+  setup() {
+    return { args }
+  },
+  template: '<nav-menu-component v-bind="args"/>',
+});
+NavMenu.args = {
+  menuList: [
+    { name: 'articles', display: 'Articles' },
+    { name: 'about', display: 'About' },
+  ],
+  onClick: (name) => console.log(`${name} is clicked!`),
+}
