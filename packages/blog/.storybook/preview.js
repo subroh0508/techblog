239b9dc73150662e4e006e0115d19d8a68385b02
subroh0508 <subroh.0508@gmail.com>
2021-07-04 18:17:31 +0900

    [fix]Set default timezone at preview.js

--- packages/blog/.storybook/preview.js
+++ packages/blog/.storybook/preview.js
@@ -1,8 +1,12 @@
 import '!style-loader!css-loader!sass-loader!@components/_global.scss';
 
+import moment from 'moment-timezone';
+
 import { app } from '@storybook/vue3';
 import { handlePushRouter } from '@mock/router';
 import NotFound from '@components/pages/global/NotFound';
+
+moment.tz.setDefault('Asia/Tokyo');
 
 app.use(handlePushRouter);
 app.component('not-found', NotFound);
